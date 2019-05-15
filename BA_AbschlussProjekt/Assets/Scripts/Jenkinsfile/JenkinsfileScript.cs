a548e4a891a115b1424571c12be7d53f385da387
MattManco <bog.chumak@gmail.com>
2019-05-15 21:34:50 +0200

    UPD hope #5

--- BA_AbschlussProjekt/Assets/Scripts/Jenkinsfile/JenkinsfileScript.cs
+++ BA_AbschlussProjekt/Assets/Scripts/Jenkinsfile/JenkinsfileScript.cs
@@ -14,7 +14,7 @@ public class JenkinsfileScript : MonoBehaviour
     [MenuItem("Custom/CI/Build Mac OS X")]
     static void PerformMacOSXBuild()
     {
-        string target_dir = APP_NAME + ".app";
+        string target_dir = APP_NAME + ".exe";
         GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 
