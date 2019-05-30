00287afa4306d60bd633444024acbad82c69f7a0
MattManco <bog.chumak@gmail.com>
2019-05-30 23:48:48 +0200

    ADD Strike Team Schlafsaal Scene

---
+++ BA_AbschlussProjekt/Assets/Strike-Team-Schlafsaal/Scripts/ImageClosure.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageClosure : MonoBehaviour
+{
+    bool active = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active && (CTRLHub.Interact || Input.GetKeyDown(KeyCode.Escape)))
+        {
+            this.gameObject.SetActive(false);
+            active = false;
+        }
+    }
+
+    public void open()
+    {
+        StartCoroutine(enableClose());
+    }
+
+
+    public IEnumerator enableClose()
+    {
+        yield return new WaitForSeconds(1f);
+        active = true;
+    }
+}
