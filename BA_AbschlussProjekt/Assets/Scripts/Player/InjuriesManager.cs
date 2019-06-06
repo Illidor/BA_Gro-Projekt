d0dda83f0cb3075e8403dca972aed45cd6b3b42f
MattManco <bog.chumak@gmail.com>
2019-06-06 19:23:33 +0200

    MRG master

---
+++ BA_AbschlussProjekt/Assets/Scripts/Player/InjuriesManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InjuriesManager : MonoBehaviour
+{
+    //private List<InstancePool.PlayerInjuries> injurieList;
+
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    injurieList = new List<InstancePool.PlayerInjuries>();
+    //}
+
+    //public List<InstancePool.PlayerInjuries> GetPlayerInjuries()
+    //{
+    //    return injurieList;
+    //}
+}
