695493845bfdefbe187fba6f4ae7e5f36e9a2525
MattManco <bog.chumak@gmail.com>
2019-06-07 14:54:58 +0200

    UPD InteractionScript w/out throwing

--- BA_AbschlussProjekt/Assets/Scripts/Player/InteractionScript.cs
+++ BA_AbschlussProjekt/Assets/Scripts/Player/InteractionScript.cs
@@ -2,53 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class InteractionScript : MonoBehaviour
 {
-    [SerializeField] [Tooltip("Max distance to objects the player is able to grab")]
-    private float grabingReach = 1.5f;
-    [SerializeField] [Tooltip("Multiplicator of final throwing strength")]
-    private float throwingStrength = 1f;
-    [SerializeField] [Tooltip("Multiplicator of overtime strength when holding down the throw button")]
-    private float throwingChargeModifier = 1f;
     [SerializeField]
-    [Tooltip("Maximum throwing strength. Needed to not overpower charge")]
-    private float maxThrowingStrength = 30f;
-    [Space]
-    [SerializeField] [Tooltip("Hand the carried object is parented to")]
-    private Transform grabingPoint;
+    [Tooltip("Max distance to objects the player is able to grab")]
+    private float grabingReach = 1.5f;
 
 
-    private Carryable carriedObject;
-    public Carryable CarriedObject
+    private ObjectInteraction usedObject;
+    public ObjectInteraction UsedObject
     {
-        get { return carriedObject; }
+        get { return usedObject; }
         set
         {
-            carriedObject = value;
-            IsCarrying = (value == null ? false : true);
+            usedObject = value;
+            IsInteracting = (value == null ? false : true);
         }
     }
 
-    public bool IsCarrying { get; set; }
-
-    public Transform GrabingPoint { get { return grabingPoint; } }
-
-
-    private float throwChargeTimer;
-
+    public bool IsInteracting { get; set; }
 
     private void Update()
     {
         if (CTRLHub.InteractDown)
             CheckInteraction();
 
-        if (IsCarrying)
-        {
-            HandleThrowing();
-            //if is carrying stuff handle use of those
-            //HandleUseObject();
-        }
+        if (IsInteracting)
+            HandledDrop();
     }
 
     private void CheckInteraction()
@@ -63,12 +45,12 @@ public class InteractionScript : MonoBehaviour
             {
                 if (IsCarrying)
                 {
-                    Debug.Log("combine " + CarriedObject.name  +" with " + interactableToInteractWith.name);
+                    Debug.Log("combine " + CarriedObject.name + " with " + interactableToInteractWith.name);
                     //CarriedObject.Combine(this, interactableToInteractWith);
 
                     interactableToInteractWith.gameObject.GetComponent<BaseInteractable>().Combine(CarriedObject.gameObject);
                 }
-                else if(interactableToInteractWith.gameObject.GetComponent<FlashbackInteraction>() == null)
+                else if (interactableToInteractWith.gameObject.GetComponent<FlashbackInteraction>() == null)
                 {
                     interactableToInteractWith.Interact(this);
                 }
@@ -80,39 +62,18 @@ public class InteractionScript : MonoBehaviour
         }
         else
         {
-            if(IsCarrying)
+            if (IsCarrying)
             {
                 CarriedObject.gameObject.GetComponent<BaseInteractable>().Use();
             }
         }
     }
 
-    private void HandleThrowing()
+    private void HandledDrop()
     {
-        if (CTRLHub.ThrowDown)
-            throwChargeTimer = Time.time;
-
         if (CTRLHub.ThrowUp)
         {
-            // time difference times time difference multiplicator times overall multiplicator
-            float finalThrowingStrength = (Time.time - throwChargeTimer) * throwingChargeModifier * throwingStrength; 
-            if (finalThrowingStrength > maxThrowingStrength)
-                finalThrowingStrength = maxThrowingStrength;
-
-            CarriedObject.Throw(this, finalThrowingStrength);
+            UsedObject.PutDown(this);
         }
     }
 }
-
-
-/*
-
- BaseInteractable interactable = hit.collider.GetComponent<BaseInteractable>();
- if (interactable != null)
-    interactable.Interact(this);
-
-    == 
- 
-hit.collider.GetComponent<BaseInteractable>()?.Interact(this);
-
- */
