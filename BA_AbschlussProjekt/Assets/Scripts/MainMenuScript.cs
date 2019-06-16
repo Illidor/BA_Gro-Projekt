4d5d613d3a54f02a3d0e7d7234711184c1760a32
MattManco <bog.chumak@gmail.com>
2019-06-16 16:25:45 +0200

    ADD MainMenu and Controlls

---
+++ BA_AbschlussProjekt/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startGameButton;
+    [SerializeField]
+    private GameObject controllsButton;
+    [SerializeField]
+    private GameObject controllsPanel;
+
+
+
+    public void onStartGameClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void onContollsClikced()
+    {
+        startGameButton.SetActive(!startGameButton.activeSelf);
+        controllsButton.SetActive(!controllsButton.activeSelf);
+        controllsPanel.SetActive(!controllsPanel.activeSelf);
+    }
+}
