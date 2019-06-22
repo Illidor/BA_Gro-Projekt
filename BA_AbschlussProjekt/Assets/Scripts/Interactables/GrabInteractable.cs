05df9bb641e671f00a73dce9366d4166c886de8e
MSI\harlekintiger <harlekintiger@aol.com>
2019-06-22 22:18:06 +0200

    UPD PutDown Method of GrabInteractable to put down objects ontop of surfaces if reasonable

--- BA_AbschlussProjekt/Assets/Scripts/Interactables/GrabInteractable.cs
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/GrabInteractable.cs
@@ -8,6 +8,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class GrabInteractable : BaseInteractable
 {
+    private readonly Vector3 putDownOffset = new Vector3(0, 0.25f, 0);
+    /// <summary>
+    /// If the player puts down on object and the surface looking at is tilted less than that much degrees, the object will be put on top of said surface (eg. a table).
+    /// Else, the object will be put down in between the player and the point he is looking at (eg. a wall)
+    /// </summary>
+    private readonly float putDownOnTopDefinitionOffsetInDegrees = 45f; 
+
     /// <summary>
     /// Able to carry object if player condition matches any of the selected
     /// </summary>
@@ -20,28 +27,25 @@ public class GrabInteractable : BaseInteractable
     [EnumFlag]
     public InteractionConditions possibleConditionsToPush = InteractionConditions.Unable;
 
+    //Sound     //please add a <summary> to serialized fields
+    [SerializeField]
+    protected float velocity;
+
+
     protected Rigidbody rigid;
     protected Rigidbody rigidbodyPulling;
 
-    //Sound
-    [SerializeField]
-    protected float velocity;
-    [SerializeField]
+    protected new Collider collider;
 
     public bool IsBeeingCarried { get; protected set; }
 
     public bool IsBeeingPulled { get; protected set; }
 
-    protected enum SoundTypes
-    {
-        pickup = 0,
-        drop = 1 //TODO: override
-    }
-
 
     protected new void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
 
         base.Awake();
     }
@@ -83,9 +87,32 @@ public class GrabInteractable : BaseInteractable
     {
         if (IsBeeingCarried)
         {
-            transform.localPosition += new Vector3(0, 0, -1.5f);
-            transform.parent = InstancePool.transform;
-            transform.position += new Vector3(0, 0.5f, 0);
+            transform.SetParent(InstancePool.transform, true);
+
+            Ray screenCenterRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(screenCenterRay, out RaycastHit raycastHit, player.GrabingReach))
+            {
+                // if the player is looking at a surface in his reach, ...
+                if (Vector3.Angle(raycastHit.normal, Vector3.up) < putDownOnTopDefinitionOffsetInDegrees)
+                {
+                    // ... and it's pointing upwards (eg a table), then put the object down obtop of it.
+                    transform.position = raycastHit.point + putDownOffset;
+                }
+                else
+                {
+                    // ... and it's not pointing upwards (eg a wall), then put the object down inbetween it and the player
+                    transform.position = raycastHit.point - screenCenterRay.direction;
+                }
+            }
+            else
+            {
+                // if the player is not looking at a surface, drop the object from the players max reach infront of him
+                transform.position = Camera.main.transform.position + screenCenterRay.direction * player.GrabingReach;
+
+                // Alternative: if the player is not looking at a surface, drop the object where the player stands like he just released the grip without caring where it lands.
+                // To implement, delete this else block
+            }
         }
         else
         {
@@ -122,7 +149,7 @@ public class GrabInteractable : BaseInteractable
         if (IsBeeingPulled)
         {
             Ray r = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-            Vector3 pointToPull = r.GetPoint(rigidbodyPulling.gameObject.GetComponent<InteractionScript>().GetReach());
+            Vector3 pointToPull = r.GetPoint(rigidbodyPulling.GetComponent<InteractionScript>().GrabingReach);
             pointToPull = new Vector3(pointToPull.x, transform.position.y, pointToPull.z);
             transform.position = Vector3.Lerp(transform.position,pointToPull, 0.3f);
         }
@@ -134,7 +161,7 @@ public class GrabInteractable : BaseInteractable
 
     }
 
-    // Wait, why is this gone?
+    // Wait, why is this gone? And if that's okey, why is it still here?
     ///// <summary>
     ///// Play a sound of a type eg. pickup, drop
     ///// </summary>
