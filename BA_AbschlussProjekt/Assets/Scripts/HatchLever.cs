06b569c7e2358561622882dd32f14f264f345078
harlekintiger/SebastianNeikes <harlekintiger@aol.com>
2019-07-10 01:10:32 +0200

    ADD hatchLever prefab, UPD hatchLever.cs

--- BA_AbschlussProjekt/Assets/Scripts/HatchLever.cs
+++ BA_AbschlussProjekt/Assets/Scripts/HatchLever.cs
@@ -3,10 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HatchLever : InteractionFoundation, ICombinable
+public class HatchLever : BaseInteractable, ICombinable
 {
     [SerializeField]
+    private HatchInteraction hatch;
+    [SerializeField]
     private LevelCombinatables[] levelCombinables;
+    [SerializeField]
+    private HingeJoint hinge;
+    [SerializeField]
+    private Sound soundToPlayOnLeverFlip;
+
+    public bool IsLeverDown { get; private set; }
+
+    private bool hasLever = false;
+    private bool alreadyOpend = false;
+
+    protected void Start()
+    {
+        hinge.transform.localRotation = Quaternion.Euler(hinge.limits.min, 0, 0);
+
+        for (int i = 0; i < levelCombinables.Length; i++)
+            levelCombinables[ i ].corespondingObjectToDisplay.SetActive(false);
+
+        soundToPlayOnLeverFlip = GetComponent<Sound>();
+    }
 
     public bool HandleCombine(InteractionScript player, BaseInteractable currentlyHolding)
     {
@@ -22,8 +43,47 @@ public class HatchLever : InteractionFoundation, ICombinable
 
     public bool Combine(InteractionScript player, BaseInteractable interactingComponent)
     {
+        for (int i = 0; i < levelCombinables.Length; i++)
+        {
+            if (levelCombinables[ i ].objectToCombineWith.Equals(interactingComponent))
+            {
+                levelCombinables[ i ].corespondingObjectToDisplay.SetActive(true);
+                hasLever = true;
 
+                player.StopUsingObject();
+                Destroy(interactingComponent.gameObject);
+
+                return true;
+            }
+        }
         return false;
+    }
+
+    public override bool CarryOutInteraction(InteractionScript player)
+    {
+        if (hasLever == false)
+            return false;
+
+        if (IsLeverDown)
+        {
+            hinge.transform.localRotation = Quaternion.Euler(hinge.limits.min, 0, 0);
+        }
+        else
+        {
+            hinge.transform.localRotation = Quaternion.Euler(hinge.limits.max, 0, 0);
+
+            if (!alreadyOpend)
+            {
+                hatch.Combine(player, this);
+                alreadyOpend = true;
+            }
+        }
+
+        soundToPlayOnLeverFlip?.PlaySound(0);
+
+        IsLeverDown = !IsLeverDown;
+
+        return true;
     }
 
 
@@ -46,5 +106,6 @@ public class HatchLever : InteractionFoundation, ICombinable
             levelCombinatable.name = "LevelCombinatable";
         }
     }
+
 }
 
