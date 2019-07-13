d435e19fb0808630f985705aebafc17b673a08ff
harlekintiger/SebastianNeikes <harlekintiger@aol.com>
2019-07-14 00:55:41 +0200

    UPD crowbar

--- BA_AbschlussProjekt/Assets/Scripts/Interactables/CarryableObjects/Crowbar.cs
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/CarryableObjects/Crowbar.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Crowbar : GrabInteractable
 {
+    [SerializeField]
+    private Sound breakSound;
+
     public void Break(InteractionScript player)
     {
         PutDown(player);
         Destroy(gameObject);
+        breakSound?.PlaySound(0);
     }
 }
