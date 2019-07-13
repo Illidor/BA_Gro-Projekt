54a99e79920ad881951f33b4b6ec36644cef5526
harlekintiger/SebastianNeikes <harlekintiger@aol.com>
2019-07-14 00:53:18 +0200

    UPD ExitDoor (renamed DoorKeyInteraction) and Crowbar

---
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/CarryableObjects/Crowbar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowbar : GrabInteractable
+{
+    public void Break(InteractionScript player)
+    {
+        PutDown(player);
+        Destroy(gameObject);
+    }
+}
