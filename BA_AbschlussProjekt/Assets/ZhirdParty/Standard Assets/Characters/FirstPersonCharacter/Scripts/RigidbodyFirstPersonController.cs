91e30cbc267362996b16b4c82734e36ca71923da
MattManco <bog.chumak@gmail.com>
2019-07-15 00:28:34 +0200

    UPD MainMenue mouselock fixed UPD Crouch anim. -> best practise set crouchTime to 100 in Inspector

--- BA_AbschlussProjekt/Assets/ZhirdParty/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
+++ BA_AbschlussProjekt/Assets/ZhirdParty/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
@@ -105,6 +106,9 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
 		// Custom
 		private bool isCrouching = false;
+        private Coroutine crouchingCR;
+        [SerializeField]
+        private float crouchTime;
 		[SerializeField] private Vector3 cameraCrouchPositionOffset = new Vector3(0f, -0.5f, 0);
 		[SerializeField] private CapsuleCollider standCollider;
 		[SerializeField] private CapsuleCollider crouchCollider;
@@ -114,8 +118,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 		[SerializeField] private float footstepSoundThreshold = 0.65f;
 		private int footstepSoundCount = 0;
 
-		// used for saving before reducing the speed with health conditions
-		private float defaultForwardSpeed;
+        // used for saving before reducing the speed with health conditions
+        private float defaultForwardSpeed;
 		private float defaultBackwardSpeed;
 		private float defaultStrafeSpeed;
 
@@ -152,7 +156,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
 				isCrouching = true;
 				crouchCollider.enabled = true;
 				standCollider.enabled = false;
-				cam.transform.localPosition = cameraCrouchPositionOffset;
+
+                if(crouchingCR != null)
+                {
+                    StopCoroutine(crouchingCR);
+                    crouchingCR = null;
+                }
+                crouchingCR = StartCoroutine(moveCamTo(cameraCrouchPositionOffset, crouchTime));
 			}
 
 			if (m_Jump == false && CrossPlatformInputManager.GetButtonUp("Crouch") && isCrouching)
@@ -160,11 +170,31 @@ namespace UnityStandardAssets.Characters.FirstPerson
 				isCrouching = false;
 				standCollider.enabled = true;
 				crouchCollider.enabled = false;
-				cam.transform.localPosition = Vector3.zero;
-			}
+
+                if (crouchingCR != null)
+                {
+                    StopCoroutine(crouchingCR);
+                    crouchingCR = null;
+                }
+                crouchingCR = StartCoroutine(moveCamTo(Vector3.zero, crouchTime));
+            }
 
 			CheckMovability();
 		}
+
+        private IEnumerator moveCamTo(Vector3 p_moveTo, float p_duration)
+        {
+            float duration = p_duration;
+
+            while(duration > 0)
+            {
+                cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, p_moveTo, 1/duration);
+
+                yield return new WaitForFixedUpdate();
+
+                duration -= Time.deltaTime;
+            }
+        }
 
 		private void FixedUpdate()
 		{
