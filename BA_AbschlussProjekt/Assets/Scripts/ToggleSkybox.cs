e0fed2943bacc4271cf0215ae225f2941cb4e94b
harlekintiger/SebastianNeikes <harlekintiger@aol.com>
2019-07-18 15:39:13 +0200

    ADD toggle for skybox night / day texture

---
+++ BA_AbschlussProjekt/Assets/Scripts/ToggleSkybox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSkybox : MonoBehaviour
+{
+    [SerializeField]
+    private Material skyboxMaterial;
+    [SerializeField]
+    private Texture dayTimeTexture;
+    [SerializeField]
+    private Texture nightTimeTexture;
+
+    public void SetDaytimeSkybox()
+    {
+        skyboxMaterial.SetTexture(3, dayTimeTexture);
+    }
+
+    public void SetNightTimeSkybox()
+    {
+        skyboxMaterial.SetTexture(3, nightTimeTexture);
+    }
+
+    
+}
