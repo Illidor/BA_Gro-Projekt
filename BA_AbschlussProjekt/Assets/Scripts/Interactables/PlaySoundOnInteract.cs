49559af443f93e995bee327c71c400dbd385103b
Sebi Laptop\harlekintiger <harlekintiger@aol.com>
2019-07-30 15:02:55 +0200

    UPD bed, added ruttleing

---
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/PlaySoundOnInteract.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Sound))]
+public class PlaySoundOnInteract : BaseInteractable
+{
+    private Sound soundToPlayOnInteract;
+
+    private new void Awake()
+    {
+        soundToPlayOnInteract = GetComponent<Sound>();
+
+        base.Awake();
+    }
+
+    public override bool CarryOutInteraction(InteractionScript player)
+    {
+        soundToPlayOnInteract?.PlaySound(0);
+        return true;
+    }
+}
