9340a9e976c356944bd3a8b172425fb1fef42dae
Yves <yvesmasullo@gmail.com>
2019-07-30 13:13:29 +0200

    UPD rocking horse animation and interaction

--- BA_AbschlussProjekt/Assets/Scripts/Interactables/ToyHorse.cs
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/ToyHorse.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class ToyHorse : BaseInteractable {
 
+    // isMoving is used to check when interaction is possible and handling the delay after the animation. It gets reseted to false from within the animation
     private bool isMoving = false;
     private float interactionTicker = 0f;
-    private float interactionThreshold = 2f;
+    private float interactionThreshold = 1.25f;
+
+    private Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (isMoving == false) {
+            interactionTicker += Time.deltaTime;
+        }
+    }
 
     public override bool CarryOutInteraction(InteractionScript player) {
         if (interactionTicker > interactionThreshold) {
             interactionTicker = 0f;
+            anim.SetTrigger("StartSeesaw");
             isMoving = true;
         }
-
         return true;
     }
 
-    private void Update() {
-        if(isMoving == false) {
-            interactionTicker += Time.deltaTime;
-        }
+    public void StopMovement() {
+        isMoving = false;
     }
 }
