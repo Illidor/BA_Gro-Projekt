215fe09d59b7aac9a86d76fd35b126e4ced7420e
Sebi Laptop\harlekintiger <harlekintiger@aol.com>
2019-07-30 14:54:13 +0200

    ADD destroyable spiderwebs

---
+++ BA_AbschlussProjekt/Assets/Scripts/DestoryOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryOnCollision : MonoBehaviour
+{
+    private float time;
+
+    private void Awake()
+    {
+        time = Time.time;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (time > 7.5f)
+            return;
+
+        Destroy(gameObject);
+    }
+}
---
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/DestoryOnInteract.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryOnInteract : BaseInteractable
+{
+    public override bool CarryOutInteraction(InteractionScript player)
+    {
+        Destroy(gameObject);
+        return true;
+    }
+}
