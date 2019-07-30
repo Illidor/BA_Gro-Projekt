750f7851b6dcfbed16db39c2def4abe3d8d07a7e
Sebi Laptop\harlekintiger <harlekintiger@aol.com>
2019-07-30 15:05:29 +0200

    UPD window.cs, coding guidelines

--- BA_AbschlussProjekt/Assets/Scripts/Interactables/Window.cs
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/Window.cs
@@ -9,8 +9,10 @@ public class Window : BaseInteractable
     private float interactionTicker = 0f;
     private float interactionThreshold = 4f;
 
-    public override bool CarryOutInteraction(InteractionScript player) {
-        if(interactionTicker > interactionThreshold) {
+    public override bool CarryOutInteraction(InteractionScript player)
+    {
+        if(interactionTicker > interactionThreshold)
+        {
             interactionTicker = 0f;
             windowSounds.PlaySound(Random.Range(0, windowSounds.clips.Count));
         }
@@ -18,7 +20,8 @@ public class Window : BaseInteractable
         return true;
     }
 
-    private void Update() {
+    private void Update()
+    {
         interactionTicker += Time.deltaTime;
     }
 }
