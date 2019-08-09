c460b034857d626f2f1f964533d364fc86cf7d8a
RobinMaechtel <robinmaechtel@gmail.com>
2019-08-09 17:33:11 +0200

    ADD random path selection

---
+++ BA_AbschlussProjekt/Assets/Scripts/SelectPuzzlePath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPuzzlePath : MonoBehaviour
+{
+    public bool SelectPath = true;
+
+    [SerializeField] GameObject PathOneCrutch;
+    [SerializeField] GameObject PathTwoLever;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SelectPath)
+        {
+            if(Random.Range(0,101) >= 50)
+            {
+                PathOneCrutch.SetActive(false);
+            }
+            else
+            {
+                PathTwoLever.SetActive(false);
+            }
+        }
+    }
+}
