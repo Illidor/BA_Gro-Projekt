5f47b84658481567df3bf3f7976fea0640e96efb
Yves <yvesmasullo@gmail.com>
2019-08-12 13:05:21 +0200

    UPD toy car ADD bed manager for handling the bed rattling

---
+++ BA_AbschlussProjekt/Assets/Scripts/BedManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedManager : MonoBehaviour
+{
+    public GameObject toyCar;
+    private int rattleCount = 0;
+    [SerializeField] Transform player;
+
+    private void BedRattle(GameObject bed) {
+        rattleCount++;
+        if(rattleCount == 3) {
+            GameObject tmpCar = Instantiate(toyCar, player.position + Vector3.up + player.forward * 0.5f, Quaternion.identity);
+            tmpCar.GetComponent<ToyCar>().CallSoundAfterDelay(0.5f);
+        }
+    }
+
+    private void OnEnable() {
+        PlaySoundOnInteract.BedRattle += BedRattle;
+    }
+
+    private void OnDisable() {
+        PlaySoundOnInteract.BedRattle -= BedRattle;
+    }
+}
--- BA_AbschlussProjekt/Assets/Scripts/Interactables/PlaySoundOnInteract.cs
+++ BA_AbschlussProjekt/Assets/Scripts/Interactables/PlaySoundOnInteract.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Pixelplacement;
 
 [RequireComponent(typeof(Sound))]
 public class PlaySoundOnInteract : BaseInteractable
 {
+    public static event UnityAction<GameObject> BedRattle;
+
     private Sound soundToPlayOnInteract;
+
 
     private new void Awake()
     {
@@ -17,6 +22,14 @@ public class PlaySoundOnInteract : BaseInteractable
     public override bool CarryOutInteraction(InteractionScript player)
     {
         soundToPlayOnInteract?.PlaySound(0);
+
+        if(DisplayName == "Bed") {
+            Tween.LocalRotation(transform, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 1f), 0.25f, 0f, Tween.EaseOut, Tween.LoopType.None, null, null, true);
+            Tween.LocalRotation(transform, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, -1f), 0.25f, 0.25f, Tween.EaseOut, Tween.LoopType.None, null, null, true);
+            Tween.LocalRotation(transform, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f), 0.25f, 0.5f, Tween.EaseOut, Tween.LoopType.None, null, null, true);
+            if (BedRattle != null)
+                BedRattle(gameObject);
+        }
         return true;
     }
 }
